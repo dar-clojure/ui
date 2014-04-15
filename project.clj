e2c70e9efc18ceb7e940a3103f0ff97231e9bc8d
Eldar Gabdullin <eldargab@gmail.com>
2014-04-15 12:45:58 +0400

    develop uikit under this project for now

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject react "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+(defproject dar/ui "0.0.1-SNAPSHOT"
+  :plugins [[dar/assets-lein "0.0.1-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[dar/assets "0.0.1-SNAPSHOT"]]}}
   :target-path "build/target"
-  :clean-targets ["build"])
+  :clean-targets ["build"]
+  :assets {:build-dir "build/assets"
+           :pre-include ["dar/ui/components/css/normalize" "dar/ui/components/css/test"]
+           :server-port 3000})
