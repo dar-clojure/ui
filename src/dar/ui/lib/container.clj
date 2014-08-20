6edb6a5e9d27e40b6d7ce58c5c09579374f3d778
Eldar Gabdullin <eldargab@gmail.com>
2014-08-20 18:27:24 +0400

    remove bad stuff

--- src/dar/ui/lib/container.clj
+++
@@ -1,9 +0,0 @@
-(ns dar.ui.lib.container)
-
-(defmacro application [& body]
-  `(binding [*app* (atom (->App {} (atom {}) nil :app))]
-     ~@body
-     @*app*))
-
-(defmacro defapp [name & body]
-  `(def ~name (application ~@body)))
