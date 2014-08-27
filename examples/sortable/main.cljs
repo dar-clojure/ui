8d6a388d6ec61b53f60a8648e8cf5b11036bd024
Eldar Gabdullin <eldargab@gmail.com>
2014-08-27 20:31:25 +0400

    save work on sortable

--- examples/sortable/main.cljs
+++ examples/sortable/main.cljs
@@ -1,28 +1,57 @@
 (ns sortable.main
   (:require [dar.ui.frp :as frp :include-macros true]))
 
-(defn event-port [ctx type transform]
-  (frp/port (fn [push]
-              (let [handler (comp push transform)]
-                (.addEventListener ctx (name type) handler)
-                (fn onkill []
-                  (.removeEventListener ctx (name type) handler))))))
+(defn event-port [ctx type init transform]
+   (frp/port (fn [push]
+               (when init
+                 (push (init)))
+               (let [handler (comp push transform)]
+                 (.addEventListener ctx (name type) handler)
+                 (fn onkill []
+                   (.removeEventListener ctx (name type) handler))))))
+
+(defn mouse-event-pos [e]
+  [(.-screenX e) (.-screenY e)])
 
 (def mouse-position
   (event-port js/window :mousemove
-    (fn [e]
-      {:screenX (.-screenX e)
-       :screenY (.-screenY e)})))
+    (fn [] [0 0])
+    mouse-event-pos))
 
+(def mouseup
+  (event-port* js/window :mouseup
+    (constantly true)))
 
+(defn initial-state [col]
+  (let [capture (new-event)
+        html (frp/<- (fn [col]
+                       (set-children col
+                         (map (fn [el]
+                                (listen el :mousedown
+                                  (to capture (fn [e]
+                                                [(key el) (mouse-event-pos e)]))))
+                           (children col))))
+               col)]
+    {:commands {:capture capture}
+     :inputs {:col html}
+     :html html
+     :methods {:capture (fn [state k]
+                          (captured-state state k))}}))
 
-(defn measure [x y]
-  (let [el (js/document.getElementById "tile")]
-    (set! (.. el -style -display) "none")
-    (js/console.log (js/document.elementFromPoint x y))
-    (set! (.. el -style -display) nil)))
+(defn captured-state [state [k pos]]
+  (-> state
+    (update-in [:commands] assoc
+      :mouse mouse-position
+      :mouseup mouseup)
+    (assoc
+      :start pos
+      :initial-state state
+      :methods captured-state-methods
+      :html (new-signal
+              (prevent-mouse-events (-> state :signals :col))))))
 
-(defn -main []
-  (.addEventListener js/document "mousemove"
-    (fn [e]
-      (measure (.-screenX e) (.-screenY e)))))
+(def captured-state-methods
+  {:mouse (fn [state pos]
+            )
+   :mouseup (fn [state _]
+              (:initial-state state))})
