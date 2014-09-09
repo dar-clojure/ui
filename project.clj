cc29bcb287a9821234ee7adbe34a3a8f4cae5f34
Eldar Gabdullin <eldargab@gmail.com>
2014-09-09 22:28:00 +0400

    adapt project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject dar/ui "0.0.1-SNAPSHOT"
   :plugins [[dar/assets-lein "0.0.1-SNAPSHOT"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                                  [dar/assets "0.0.1-SNAPSHOT"]]
-                   :source-paths ["examples"]}}
+  :profiles {:dev {:source-paths ["examples"]}}
   :target-path "build/target"
   :clean-targets ["build"]
   :assets {:build-dir "build/assets"
